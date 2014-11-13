1a2759a5eb0e00ee16889ebbfbca4bf7551f1474
Ashok502 <ashokkmr098@gmail.com>
2014-11-13 13:00:59 +0530

    deployed the application to vpsdime server successfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,13 +39,13 @@ class User < ActiveRecord::Base
   validates :skills, :presence => { :if => :layout_work_required? }
   validates :company_name,:position, :presence => { :if => :company_name_required? }
   validates :schedule, :presence => { :if => :schedule_required? }
-  after_create :subscriber
+#  after_create :subscriber
 
   has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/photo_icon.jpg"
 
-  def subscriber
-    Winner.create_subscriber(self).deliver
-  end
+#  def subscriber
+#    Winner.create_subscriber(self).deliver
+#  end
 
   #  define_index do
   #    indexes name
